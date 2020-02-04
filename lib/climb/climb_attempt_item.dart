d0609a341a058688c602877022565f5885ac1244
Meebuhs <alexeksharpe@gmail.com>
2020-02-04 19:49:46 +1000

    Fix hour format in attempt timestamp

--- lib/climb/climb_attempt_item.dart
+++ lib/climb/climb_attempt_item.dart
@@ -15,71 +15,91 @@ class AttemptItem extends StatelessWidget {
         key: Key(attempt.id),
         direction: DismissDirection.endToStart,
         background: Container(
-            color: Colors.red,
+            color: Theme.of(context).errorColor,
             child: (Container(
               child: Padding(
-                padding: EdgeInsets.fromLTRB(0.0, 0.0, UIConstants.SMALLER_PADDING, 0.0),
-                child: Icon(Icons.delete_forever, color: Colors.white),
+                padding: EdgeInsets.only(right: UIConstants.SMALLER_PADDING),
+                child: Icon(Icons.delete_forever, color: Colors.black),
               ),
               alignment: Alignment.centerRight,
             ))),
         onDismissed: (direction) => ClimbRepo.getInstance().deleteAttempt(attempt.id, climbId),
-        child: ListTile(
-            title: Text(
-              "${formatDate(attempt.timestamp.toDate())} ${attempt.sendType}",
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(fontSize: UIConstants.STANDARD_FONT_SIZE, color: Colors.pinkAccent),
-            ),
-            subtitle: Text(attempt.notes),
-            trailing: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[_showWarmupTick(), _showDownclimbedTick()],
-            )));
+        child: _buildAttempt(context));
   }
 
-  Widget _showWarmupTick() {
+  Widget _buildAttempt(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(UIConstants.SMALLER_PADDING),
+        child: Container(
+            padding: EdgeInsets.all(UIConstants.SMALLER_PADDING),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(UIConstants.CARD_BORDER_RADIUS)),
+              color: Theme.of(context).cardColor,
+            ),
+            child: Row(children: <Widget>[
+              Expanded(
+                  child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+                Text(
+                  "${formatDate(attempt.timestamp.toDate())} - ${attempt.sendType}",
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).accentTextTheme.subtitle2,
+                ),
+                Text(
+                  attempt.notes,
+                  style: Theme.of(context).accentTextTheme.caption,
+                )
+              ])),
+              Container(
+                  padding: EdgeInsets.only(left: UIConstants.STANDARD_PADDING),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[_showWarmupTick(context), _showDownclimbedTick(context)],
+                  )),
+            ])));
+  }
+
+  Widget _showWarmupTick(BuildContext context) {
     return Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
-      Text(
-        "W: ",
-        style: TextStyle(fontSize: UIConstants.STANDARD_FONT_SIZE, color: Colors.grey),
-      ),
+      Text("W: ", style: Theme.of(context).accentTextTheme.bodyText2),
       (attempt.warmup)
           ? Icon(
               Icons.check,
-              color: Colors.grey,
-              size: UIConstants.STANDARD_FONT_SIZE,
+              color: Theme.of(context).accentColor,
+              size: Theme.of(context).accentTextTheme.bodyText2.fontSize,
             )
           : Icon(
               Icons.close,
-              color: Colors.grey,
-              size: UIConstants.STANDARD_FONT_SIZE,
+              color: Theme.of(context).accentColor,
+              size: Theme.of(context).accentTextTheme.bodyText2.fontSize,
             )
     ]);
   }
 
-  Widget _showDownclimbedTick() {
+  Widget _showDownclimbedTick(BuildContext context) {
     return Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
       Text(
         "D: ",
-        style: TextStyle(fontSize: UIConstants.STANDARD_FONT_SIZE, color: Colors.grey),
+        style: Theme.of(context).accentTextTheme.bodyText2,
       ),
       (attempt.downclimbed)
           ? Icon(
               Icons.check,
-              color: Colors.grey,
-              size: UIConstants.STANDARD_FONT_SIZE,
+              color: Theme.of(context).accentColor,
+              size: Theme.of(context).accentTextTheme.bodyText2.fontSize,
             )
           : Icon(
               Icons.close,
-              color: Colors.grey,
-              size: UIConstants.STANDARD_FONT_SIZE,
+              color: Theme.of(context).accentColor,
+              size: Theme.of(context).accentTextTheme.bodyText2.fontSize,
             )
     ]);
   }
 
   String formatDate(DateTime time) {
     String ampm = (time.hour < 12) ? "AM" : "PM";
-    return "${time.day}/${time.month} ${time.hour % 12}:${time.minute.toString().padLeft(2, '0')} $ampm";
+    int hour =
+        (time.hour <= 12) ? (time.hour == 0 || time.hour == 12) ? 12 : time.hour : time.hour % 12;
+    return "${time.day}/${time.month} $hour:${time.minute.toString().padLeft(2, '0')} $ampm";
   }
 }
