2aba96a33e1b6b3795715f55e38f3a4963bcaf5f
Meebuhs <alexeksharpe@gmail.com>
2020-02-08 13:10:18 +1000

    Group attempts by date in history

--- lib/history/history_view.dart
+++ lib/history/history_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:sendrax/history/history_attempt_item.dart';
+import 'package:sendrax/models/attempt.dart';
 import 'package:sendrax/util/constants.dart';
 
 import 'history_bloc.dart';
@@ -58,24 +60,71 @@ class HistoryWidget extends StatelessWidget {
           } else {
             return Column(children: <Widget>[
               Expanded(
-                child: _buildAttempts(state, context),
+                child: _buildGroupedList(state, context),
               )
             ]);
           }
         });
   }
 
-  Widget _buildAttempts(HistoryState state, BuildContext context) {
+  Widget _buildGroupedList(HistoryState state, BuildContext context) {
+    List<DateTime> datesToBuild = _generateDates(state);
     return ListView.builder(
       padding: EdgeInsets.all(UIConstants.SMALLER_PADDING),
       itemBuilder: (context, index) {
-        return _buildAttempt(context, state, index);
+        return _buildDateCard(context, state, datesToBuild, index);
       },
-      itemCount: state.attempts.length,
+      itemCount: datesToBuild.length,
     );
   }
 
-  Widget _buildAttempt(BuildContext context, HistoryState state, int index) {
-    return AttemptItem(attempt: state.attempts[index]);
+  List<DateTime> _generateDates(HistoryState state) {
+    List<DateTime> dates = <DateTime>[];
+    DateTime startDate = state.attempts.last.timestamp.toDate();
+    DateTime endDate = state.attempts.first.timestamp.toDate();
+    DateTime currentDate = DateTime(startDate.year, startDate.month, startDate.day);
+    while (currentDate.isBefore(endDate)) {
+      dates.insert(0, currentDate);
+      currentDate = currentDate.add(Duration(days: 1));
+    }
+    return dates;
+  }
+
+  Widget _buildDateCard(BuildContext context, HistoryState state, List<DateTime> dates, int index) {
+    List<Attempt> attemptsOnDate = List.from(state.attempts.where((attempt) =>
+        (attempt.timestamp.toDate().difference(dates[index]) > Duration() &&
+            attempt.timestamp.toDate().difference(dates[index]) < Duration(days: 1))));
+    if (attemptsOnDate.isNotEmpty) {
+      return Card(
+          child: Column(children: <Widget>[
+        Row(children: <Widget>[
+          Expanded(
+              child: Padding(
+            padding: EdgeInsets.all(1.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.vertical(top: Radius.circular(UIConstants.CARD_BORDER_RADIUS)),
+                color: Theme.of(context).accentColor,
+              ),
+              padding: EdgeInsets.all(UIConstants.SMALLER_PADDING),
+              child: Text(DateFormat('EEEE d/M').format(dates[index]),
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).primaryTextTheme.subtitle2),
+            ),
+          )),
+        ]),
+        Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:
+                attemptsOnDate.map((attempt) => _buildAttempt(context, state, attempt)).toList())
+      ]));
+    } else {
+      return Column();
+    }
+  }
+
+  Widget _buildAttempt(BuildContext context, HistoryState state, Attempt attempt) {
+    return AttemptItem(attempt: attempt);
   }
 }
