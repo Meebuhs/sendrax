870ff78cd5f635e86c48dcbfb24106f92a0f9fe7
Meebuhs <alexeksharpe@gmail.com>
2020-02-19 17:44:08 +1000

    Set firestore cache size
    Ensures that with all attempts loaded, a location's climbs are loaded once and then retrieved from the cache

--- lib/models/firebase_repo.dart
+++ lib/models/firebase_repo.dart
@@ -9,7 +9,7 @@ class FirebaseRepo {
   factory FirebaseRepo.getInstance() {
     if (_instance == null) {
       _instance = FirebaseRepo._internal(Firestore.instance);
-      _instance.firestore.settings(persistenceEnabled: true);
+      _instance.firestore.settings(persistenceEnabled: true, cacheSizeBytes: 2147483647);
     }
     return _instance;
   }
